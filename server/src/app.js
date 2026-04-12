32d82e0ce409cc971d8ea13b6f4ab58ab90636a3
BilalKhalifa <bilalkhalifa9988@gmail.com>
2026-04-12 21:39:57 +0530

    feat(server): add express app bootstrap and health endpoint

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const healthRoutes = require("./routes/health.routes.js");
+const errorMiddleware = require("./middlewares/error.middleware.js");
+const analyzeRoutes = require("./routes/analyze.routes.js");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/health", healthRoutes);
+
+app.use("/analyze", analyzeRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+})
+
+app.use(errorMiddleware);
+
+module.exports = app;
