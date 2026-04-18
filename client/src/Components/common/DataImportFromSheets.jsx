5a36240e1b003892ae0de9bbb29fc211020043bc
BilalKhalifa <bilalkhalifa9988@gmail.com>
2026-04-18 23:36:25 +0530

    Starting ti build the Google Sheet Link Component

---
+++ client/src/Components/common/DataImportFromSheets.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function DataImportFromSheets() {
+    return (
+        <div>
+            <h1>Data Import From Sheets</h1>
+        </div>
+    );
+}
+
+export default DataImportFromSheets;
